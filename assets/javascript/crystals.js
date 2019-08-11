d6c5fafa841397467ae72fa947e395bf84db35fc
Alan Leverenz <awleverenz@aol.com>
2019-08-11 11:24:48 -0400

    added play and reset buttons

--- assets/javascript/crystals.js
+++ assets/javascript/crystals.js
@@ -8,8 +8,26 @@ $(document).ready(function() {
     var crystalNumArr = [];
     var randomNum = 0;
     var totalScore = 0;
+    var reset = false;
+
+    // hide play, reset and show-instructions buttons when loading
+
+    $(".hide-instructions").hide();
+    $(".instructions").hide();
+    $(".play").hide();
+    $(".reset").show();
 
     // FUNCTIONS TO SET NUMBERS =====================================
+
+    // generate random target number
+    var setRandomNum = function () {
+        while (randomNum < 19) {
+            randomNum = Math.floor(Math.random() * 121) + 1; // control the range of possible integers
+        }
+        console.log("Random Num = " + randomNum);
+        // update the randomNum html 
+        $("#random-value").text(randomNum);
+    } // end var setRandomNum
 
     // generate random numbers for the crystals
     var setRandomArr = function () { 
@@ -20,64 +38,92 @@ $(document).ready(function() {
             }
             crystalNumArr.push(num);
         } console.log(crystalNumArr);
-    } 
+    } // end define setRandomArr
 
-    setRandomArr(); // call the setRandomArr function
+    // playAgain function that initializes score, message, and computes random numbers
+    var playAgain = function () {
+        randomNum = 0;
+        crystalNumArr = [];
+        setRandomNum();
+        $("#score-value").text("0");
+        $("#message").text("");
+        setRandomArr();
+    } // end var playAgain
 
-    // generate random target number
-    while (randomNum < 19) {
-        randomNum = Math.floor(Math.random() * 121) + 1;
-    }
-    console.log("Random Num = " + randomNum);
-
-    // update the randomNum html 
-    $("#random-value").text(randomNum);
+    // Call functions when page loads
+    setRandomArr(); // call the setRandomArr function for crystal values
+    setRandomNum(); // call the setRandomNum to set random number
 
 
     // EVENTS ========================================================
 
-    // click event captured
+    // click event captured for all buttons
     $(document).on("click", "button", function () {
-        // saving the clicked button
+        // saving the clicked button id and value
         var id = $(this).attr("id");
         var value = $(this).attr("value");
         console.log("id = " + id);
         console.log("name = " + value);
 
-        // if crystal get value from the crystalNumArr and add to the total score and update the html
+        // if crystal button get value from the crystalNumArr
+        // add to the total score and update the html
         if (value === "crystal") {
             index = parseInt(id);
             totalScore = totalScore + crystalNumArr[index];
             $("#score-value").text(totalScore);
 
-        console.log("Total Score = " + totalScore);
+            console.log("Total Score = " + totalScore);
 
-        // test if score matches the random number (win) or is great (lose)
-        if ( totalScore === randomNum ) {
-            console.log("I WIN!!!");
-            wins++;
-            $("#wins").text(wins);
-            reset = true;
-        } // end if game is won
+            // test if score matches the random number (win) or greater (lose)
+            if ( totalScore === randomNum ) {
+                wins++;
+                reset = true;
+                // update the page
+                $("#wins").text(wins);
+                $("#message").text("You won!");
+                console.log("YOU WON!!!");
+            } // end if game is won
 
-        if ( totalScore > randomNum ) {
-            console.log("I LOST.");
-            losses++;
-            $("#losses").text(losses);
-            reset = true;
-        } // end if game is lost
+            if ( totalScore > randomNum ) {
+                losses++;
+                reset = true;
+                $("#losses").text(losses);
+                $("#message").text("You lost!");
+                console.log("YOU LOST.");
+            } // end if game is lost
 
-        // reset the game if won or lost - new randomNum, and new crystal values
-        // append PLAY AGAIN button and RESET, plus message
+            // id game won/lost, show PLAY and RESET buttons
+            if (reset) {
+                $(".play").show();
+                $(".reset").show();
+            } // end reset
 
+        } // end if crystal button clicked
 
-    } // end if crystal was clicked
+        // Hide instructions button clicked
+        else if (value === "hide-instructions") {
+            $(".instructions").hide();
+            $(".hide-instructions").hide();
+            $(".show-instructions").show();
+        } 
 
+        // Show instructions button clicked
+        else if (value === "show-instructions") {
+            $(".instructions").show();
+            $(".hide-instructions").show();
+            $(".show-instructions").hide();
+        } 
+        
+        // Play button clicked
+        else if (value === "play-game") {
+            playAgain();
+        }
 
+        // Reset button clicked
+        else if (value === "reset-game") {
+            location.reload();
+        }
 
-    });
-
-
-
+    }); // end click button event
 
 }); // end of document.ready
